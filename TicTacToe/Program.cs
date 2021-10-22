58cd915f571ee6e9a8f81393113a1b7fc31d3898
Bob K <kearnsr135@gmail.com>
2021-10-22 16:52:31 -0400

    Update program

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,24 +8,72 @@ namespace TicTacToe
 {
     class Program
     {
+        static char[,] playField =
+        {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'}
+        };
+
         static void Main(string[] args)
         {
+            int player = 2;
+            int input = 0;
+            bool inputCorrect = true;
+
             SetField();
             Console.ReadKey();
+
+            do
+            {
+                if (player == 2)
+                {
+                    player = 1;
+                }
+                else if (player == 1)
+                {
+                    player = 2;
+                }
+
+
+
+            } while (true);
         }
 
         public static void SetField()
         {
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ",1, 2, 3); // replace numbers with variables
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", playField[0, 0], playField[0, 1], playField[0, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3); // replace numbers with variables
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", playField[1, 0], playField[1, 1], playField[1, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3); // replace numbers with variables
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", playField[2, 0], playField[2, 1], playField[2, 2]);
             Console.WriteLine("     |     |     ");
+        }
 
+        public static void EnterXorO(int player, int input)
+        {
+            char playerSign = ' ';
+
+            if (player == 1)
+                playerSign = 'X';
+            else if (player == 2)
+                playerSign = 'O';
+
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+            }
         }
     }
 }
